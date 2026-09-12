e7af91859fe29ea0300cf722b4fd1dce41e0dd23
xisain <husenabs232bgr@gmail.com>
2026-09-13 06:43:12 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
+}
